c502fef6115ac127f19791b650e7aecbb0891cdc
NHWK-cpu <hafizhammarmuflih@gmail.com>
2024-08-28 15:25:08 +0700

    menyesuaikan finance calculator agar dapat menghitung pengeluaran lain(tugas)

--- Finance calculator.py
+++ Finance calculator.py
@@ -1,6 +1,24 @@
 import os
 import time
 
+def pengeluaran_eksternal() -> dict:
+    pengeluaran = {}
+
+    while True:
+        try:
+            jenis_pengeluaran: str = input('Pengeluaran apakah itu? ')
+            jumlah_pengeluaran: float = float(input('Berapa jumlah uang yang dikeluarkan dalam satu bulan? '))
+            pengeluaran.update({jenis_pengeluaran:jumlah_pengeluaran})
+        except:
+            print('anda harus masukkan data dengan benar!')
+            continue
+
+        option = input('(y/n) apakah ada lagi? ')
+        if option == 'n':
+            os.system('cls')
+            break
+    return pengeluaran
+
 def calculate_finance(pendapatan_perbulan: float, persentase_pajak: float, currency: str) -> None:
     # Perbulan
     pajak_perbulan: float = pendapatan_perbulan * (persentase_pajak/100)
@@ -24,6 +42,33 @@ def calculate_finance(pendapatan_perbulan: float, persentase_pajak: float, curre
     print(f'Gaji bersih pertahun: {currency} {gaji_bersih_pertahun:,.0f}')
     print('~'*25)
 
+    option:str = input('(y/n) apakah anda punya pengeluaran lain yang ingin dihitung? ')
+    if option == 'y':
+        pengeluaran_lain:dict = pengeluaran_eksternal()
+        total_pengeluaran_lain: float = 0
+        
+        print('~'*25)
+        print(f'Gaji kotor perbulan: {currency} {pendapatan_perbulan:,.0f}')
+        print(f'Persentase pajak: {persentase_pajak:,.0f}%')
+        print(f'Pajak perbulan: {currency} {pajak_perbulan:,.0f}')
+        print(f'Gaji bersih perbulan: {currency} {gaji_bersih_perbulan:,.0f}')
+
+        print('-'*10)
+
+        print(f'Gaji kotor pertahun: {currency} {pendapatan_pertahun:,.0f}')
+        print(f'Pajak pertahun: {currency} {pajak_pertahun:,.0f}')
+        print(f'Gaji bersih pertahun: {currency} {gaji_bersih_pertahun:,.0f}')
+        print('~'*25)
+
+        for i,j in pengeluaran_lain.items():
+            print(f'Pengeluaran {i}: {currency} {float(j):,.0f}')
+            total_pengeluaran_lain = total_pengeluaran_lain + j
+
+        print('-'*10)
+        print(f'Total pengeluaran lain: {currency} {total_pengeluaran_lain:,.0f}')
+        print(f'Total uang tersisah jika dihitung dengan pengeluaran lain (perbulan): {currency} {(gaji_bersih_perbulan - total_pengeluaran_lain):,.0f}')
+        print('~'*25)
+
 def main() -> None:
     while True:
         try:
@@ -35,7 +80,7 @@ def main() -> None:
 
             calculate_finance(gaji,pajak,currency)
             
-            repeat = input('Apakah anda ingin mengulangi? (y/n) ')
+            repeat = input('(y/n) Apakah anda ingin mengulangi? ')
             os.system('cls')
             if repeat == 'n':
                 break
